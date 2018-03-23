b1d0f8e26b8db4598b75428c5aaf8b656c2da819
Alex Gorbenko <gorbenkoav@gmail.com>
2018-03-23 19:16:55 +0200

    101. Symmetric Tree iterative solution

---
+++ src/ag/leetcode/problems/trees/SymmetricTree.java
@@ -0,0 +1,78 @@
+package ag.leetcode.problems.trees;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ *
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ *
+ *      1
+ *     / \
+ *   2    2
+ *  / \  / \
+ * 3  4 4  3
+ * But the following [1,2,2,null,3,null,3] is not:
+ *    1
+ *   / \
+ *  2   2
+ *   \   \
+ *   3    3
+ * Note:
+ * Bonus points if you could solve it both recursively and iteratively.
+ *
+ * https://leetcode.com/problems/symmetric-tree/description/
+ */
+public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Queue<TreeNode> left = new LinkedList<>();
+        left.add(root.left);
+        Queue<TreeNode> right = new LinkedList<>();
+        right.add(root.right);
+
+        int size;
+        TreeNode leftNode;
+        TreeNode rightNode;
+        while (!left.isEmpty()) {
+            size = left.size();
+            for (int i = 0; i < size; i++) {
+                leftNode = left.remove();
+                rightNode = right.remove();
+
+                if (!isEqual(leftNode, rightNode)) {
+                    return false;
+                }
+
+                if (leftNode != null) {
+                    Collections.addAll(left, leftNode.left, leftNode.right);
+                }
+
+                if (rightNode != null) {
+                    Collections.addAll(right, rightNode.right, rightNode.left);
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private final boolean isEqual(TreeNode left, TreeNode right) {
+        return (left == null && right == null) || (left != null && right != null && left.val == right.val);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
